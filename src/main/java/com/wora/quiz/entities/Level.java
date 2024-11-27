c7ab67219ba39d445ed1325d29ac91e6f92e79be
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-27 14:45:27 +0100

    Level entity

--- src/main/java/com/wora/quiz/entities/Level.java
+++ src/main/java/com/wora/quiz/entities/Level.java
@@ -1,4 +1,20 @@
 package com.wora.quiz.entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Level {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String description;
+    private int maxPoints;
+    private int minPoints;
+
+    @OneToMany(mappedBy = "niveau")
+    private List<Sujet> sujets;
 }
+
