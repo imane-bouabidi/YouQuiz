83257457eec0dbd54f889a0e5e76a282c8a8f1e0
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-27 14:42:23 +0100

    Answer entity

--- src/main/java/com/wora/quiz/entities/Answer.java
+++ src/main/java/com/wora/quiz/entities/Answer.java
@@ -1,4 +1,16 @@
 package com.wora.quiz.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
 }
