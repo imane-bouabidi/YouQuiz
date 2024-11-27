3e1eb2a1766e792eb7610b879825f375f7fa3a4e
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-27 14:08:32 +0100

    project initializr

---
+++ src/test/java/com/wora/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
