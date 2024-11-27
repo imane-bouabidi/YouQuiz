0c309239210ef8a80cc16f60efdbb41b2f1d2b72
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-27 14:37:58 +0100

    Student entity

--- src/main/java/com/wora/quiz/entities/Student.java
+++ src/main/java/com/wora/quiz/entities/Student.java
@@ -1,4 +1,19 @@
 package com.wora.quiz.entities;
 
-public class Student {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@DiscriminatorValue("Etudiant")
+public class Student extends User {
+    private java.util.Date dateInscription;
+
+    @OneToMany(mappedBy = "etudiant")
+    private List<PassageTest> passageTests;
+
+
 }
+
