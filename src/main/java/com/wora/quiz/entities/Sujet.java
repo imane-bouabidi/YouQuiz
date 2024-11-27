c9216e65d3c9fdbbadae088916c726ee3724d105
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-27 14:40:59 +0100

    Sujet entity

--- src/main/java/com/wora/quiz/entities/Sujet.java
+++ src/main/java/com/wora/quiz/entities/Sujet.java
@@ -1,4 +1,28 @@
 package com.wora.quiz.entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Sujet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String titre;
+
+    @ManyToMany(mappedBy = "sujets")
+    private List<Quiz> tests;
+
+    @ManyToOne
+    @JoinColumn(name = "niveau_id")
+    private Level level;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_sujet_id")
+    private Sujet parentSujet;
+
+    @OneToMany(mappedBy = "parentSujet")
+    private List<Sujet> subSubjects;
 }
+
