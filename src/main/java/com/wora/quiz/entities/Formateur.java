f05b7ad896a094ba1be8d4cebe774c4672e790f7
imane-bouabidi <imaneyoucode@gmail.com>
2024-11-27 14:37:03 +0100

    Formateur entity

--- src/main/java/com/wora/quiz/entities/Formateur.java
+++ src/main/java/com/wora/quiz/entities/Formateur.java
@@ -1,4 +1,20 @@
 package com.wora.quiz.entities;
 
-public class Formateur {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@DiscriminatorValue("Formateur")
+public class Formateur extends User {
+    private String specialite;
+
+    @OneToMany(mappedBy = "formateur")
+    private List<Test> tests;
+
+    public Formateur() {}
+
 }
+
